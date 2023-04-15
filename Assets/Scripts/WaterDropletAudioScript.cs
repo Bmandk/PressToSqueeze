c14cd31382a825564ba6709503990fed6f627b10
EsbernTK <esberntk@gmail.com>
2023-04-15 22:27:54 +0200

    added audio to player

---
+++ Assets/Scripts/WaterDropletAudioScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDropletAudioScript : MonoBehaviour
+{   
+
+    public List<AudioClip> waterDropletAudioClips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (waterDropletAudioClips.Count > 0)
+        {
+            AudioSource.PlayClipAtPoint(waterDropletAudioClips[Random.Range(0, waterDropletAudioClips.Count)], transform.position);
+        }
+    }
+}
