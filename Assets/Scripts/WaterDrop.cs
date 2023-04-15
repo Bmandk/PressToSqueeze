566b70101db55f7d6bd308698b4f6404d0f0bf12
EsbernTK <esberntk@gmail.com>
2023-04-15 23:28:02 +0200

    added audio to waterdrops

--- Assets/Scripts/WaterDrop.cs
+++ Assets/Scripts/WaterDrop.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class WaterDrop : MonoBehaviour
 {
+
+    public List<AudioClip> waterDropletAudioClips;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") || other.CompareTag("WaterDrop") || other.CompareTag("Water"))
@@ -13,6 +15,11 @@ public class WaterDrop : MonoBehaviour
         var waterReceiver = other.GetComponent<WaterReceiver>();
         if (waterReceiver != null) 
             waterReceiver.AddWater(1);
+
+        if (waterDropletAudioClips.Count > 0)
+        {
+            AudioSource.PlayClipAtPoint(waterDropletAudioClips[UnityEngine.Random.Range(0, waterDropletAudioClips.Count)], transform.position);
+        }
 
         Destroy(gameObject);
     }
