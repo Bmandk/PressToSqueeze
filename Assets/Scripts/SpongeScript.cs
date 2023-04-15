88aa25250ec0795e7b6108d76ce439a06a0fce44
Jonathan <jonathan_hertz2@live.dk>
2023-04-15 12:53:51 +0200

    Basic sponge generation

--- Assets/Scripts/SpongeScript.cs
+++ Assets/Scripts/SpongeScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,13 @@ using UnityEngine.Serialization;
 public class SpongeScript : MonoBehaviour
 {
     public int verticeNumber;
+    public AnimationCurve topSqueezeCurve;
+    public AnimationCurve topUnSqueezeCurve;
+    public AnimationCurve bottomSqueezeCurve;
+    public AnimationCurve bottomUnSqueezeCurve;
+    public AnimationCurve squeezeCurve;
+    [Range(0, 1)]
+    public float squeezeValue;
 
     private MeshFilter _meshFilter;
 
@@ -23,16 +31,24 @@ public class SpongeScript : MonoBehaviour
         
         // Populate mesh with plane vertices
         List<Vector3> vertices = new List<Vector3>();
-        
+        List<Vector2> uvs = new List<Vector2>();
+
         // Insert vertices at the top and bottom of the plane based on subdivisions.
         for (int i = 0; i < verticeNumber; i++)
         {
             // Calculate the percentage of the way through the plane we are
-            float percentage = (float) i / (float) verticeNumber;
+            float percentage = (float) i / (float)(verticeNumber - 1);
+
+            float s = squeezeCurve.Evaluate(squeezeValue);
+            float topSqueezeValue = Mathf.Lerp(topUnSqueezeCurve.Evaluate(percentage), topSqueezeCurve.Evaluate(percentage), s);
+            float bottomSqueezeValue = Mathf.Lerp(bottomUnSqueezeCurve.Evaluate(percentage), bottomSqueezeCurve.Evaluate(percentage), s);
             
             // Insert vertex at the top and bottom of the plane
-            vertices.Add(new Vector3(percentage, 0.5f, 0f));
-            vertices.Add(new Vector3(percentage, -0.5f, 0f));
+            vertices.Add(new Vector3(percentage, 1f - topSqueezeValue, 0f));
+            uvs.Add(new Vector2(percentage, 1f));
+            
+            vertices.Add(new Vector3(percentage, 0 + bottomSqueezeValue, 0f));
+            uvs.Add(new Vector2(percentage, 0));
         }
         
         List<int> triangles = new List<int>();
@@ -51,10 +67,16 @@ public class SpongeScript : MonoBehaviour
 
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
         
         // Insert mesh into mesh filter
         _meshFilter.mesh = mesh;
+    }
+
+    private void OnValidate()
+    {
+        GenerateMesh();
     }
 }
