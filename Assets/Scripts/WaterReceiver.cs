cbffaedc6548b020913b2e91b6b29fdf9f9aa180
Jonathan <jonathan_hertz2@live.dk>
2023-04-16 13:37:50 +0200

    Win screen

--- Assets/Scripts/WaterReceiver.cs
+++ Assets/Scripts/WaterReceiver.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class WaterReceiver : MonoBehaviour
 {
@@ -12,6 +14,8 @@ public class WaterReceiver : MonoBehaviour
     public GameObject[] sprites;
     public int dropsPerSprite = 5;
     public int currentDrops = 0;
+    public Image image;
+    public AnimationCurve fadeImageCurve;
     private int index = -1;
 
     public void AddWater(int amount)
@@ -24,11 +28,31 @@ public class WaterReceiver : MonoBehaviour
             if (index >= 0)
                 sprites[index].SetActive(false);
             index++;
-            if (index >= sprites.Length)
+            if (index >= sprites.Length - 1)
             {
                 index = sprites.Length - 1;
+                WinGame();
             }
             sprites[index].SetActive(true);
         }
+    }
+
+    private void WinGame()
+    {
+        StartCoroutine(FadeImage());
+    }
+
+    private IEnumerator FadeImage()
+    {
+        image.gameObject.SetActive(true);
+        float startTime = Time.timeSinceLevelLoad;
+        while (Time.timeSinceLevelLoad - startTime < fadeImageCurve.keys[fadeImageCurve.length - 1].time)
+        {
+            float percentage = (Time.timeSinceLevelLoad - startTime);
+            image.color = new Color(image.color.r, image.color.g, image.color.b, fadeImageCurve.Evaluate(percentage));
+            yield return null;
+        }
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
