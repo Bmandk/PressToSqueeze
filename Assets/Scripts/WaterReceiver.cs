fae3e22b8e578979553c988d1a286df5ae77a5d4
EsbernTK <esberntk@gmail.com>
2023-04-16 13:58:34 +0200

    added win

--- Assets/Scripts/WaterReceiver.cs
+++ Assets/Scripts/WaterReceiver.cs
@@ -18,6 +18,7 @@ public class WaterReceiver : MonoBehaviour
     public AnimationCurve fadeImageCurve;
     private int index = -1;
     public AudioClip upgradeClip;
+    public AudioClip winClip;
 
     public void AddWater(int amount)
     {
@@ -41,6 +42,7 @@ public class WaterReceiver : MonoBehaviour
 
     private void WinGame()
     {
+        AudioSource.PlayClipAtPoint(winClip, transform.position);
         StartCoroutine(FadeImage());
     }
 
